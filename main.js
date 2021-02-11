a02088458d9a1140e744be0ef5ae7c5db943fb2a
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-02-11 13:20:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function add_user()
+{
+    player_1 = document.getElementById("player1_name_input").value;
+    player_2 = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player_1", player_1);
+    localStorage.setItem("player_2", player_2);
+    window.location="math_quiz.html";
+}
