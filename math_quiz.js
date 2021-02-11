c90d4909d02e34bc5572ec6f380cc82332a6b57e
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-02-11 14:25:12 +0530

    Add files via upload

--- math_quiz.js
+++ math_quiz.js
@@ -24,3 +24,49 @@ function send() {
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 }
+
+question_turn = "player1_name";
+answer_turn = "player2_name";
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lowercase -"+answer);
+
+    if(answer == word)
+    {
+        if(answer_turn == "player1_name")
+        {
+            player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else
+        {
+            player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = player2_score; 
+        }
+    }
+    if(question_turn == "player1_name")
+        {
+            question_turn = "player2_name"
+            document.getElementById("player_question").innerHTML = "question turn - "+ player2_name ; 
+        }
+        else
+        {
+            question_turn = "player1_name"
+            document.getElementById("player_question").innerHTML = "question turn - "+ player1_name ; 
+        }
+        
+        if(answer_turn == "player1_name")
+        {
+            answer_turn = "player2_name"
+            document.getElementById("player_answer").innerHTML = "answer turn - "+ player2_name ; 
+        }
+        else
+        {
+            answer_turn = "player1_name"
+            document.getElementById("player_answer").innerHTML = "answer turn - "+ player1_name ; 
+        }
+
+        document.getElementById("output").innerHTML = "";
+}
